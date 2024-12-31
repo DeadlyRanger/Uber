3828bf87cf59d53cc5094071bddfb1928c36a33a
Your Name <you@example.com>
2024-12-31 18:55:21 +0530

    user-auth completed

--- Backend/app.js
+++ Backend/app.js
@@ -4,24 +4,26 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 const connectToDb = require('./db/db');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 connectToDb();
 
 
 
 
- app.use(cors()); 
-    app.use(express.json());
-    app.use(express.urlencoded({extended:true}));
- 
- 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
-app.get('/', (req,res) =>{
+
+
+app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.use('/users',userRoutes);
+app.use('/users', userRoutes);
 
 
 module.exports = app;

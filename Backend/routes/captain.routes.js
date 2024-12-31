8149c2ba04d29fc1e79e9dcf2a04d8b52b185f07
Your Name <you@example.com>
2024-12-31 20:36:06 +0530

    captain simple auth done

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -1,6 +1,7 @@
  const express = require('express');
  const router = express.Router ();
 const  {body } = require('express-validator');
+const authMiddleware = require('../middlewares/auth.middleware');
 const captainController = require('../controllers/captain.controller');
 
 router.post('/register',[ 
@@ -17,7 +18,18 @@ router.post('/register',[
  captainController.registerCaptain);
 
 
+ router.post('/login',[
+     body('email').isEmail().withMessage('Invalid email'),
+     body('password').isLength({min:6}).withMessage('Password must be atleast 6 characters')
 
+ ],
+captainController.loginCaptain
+);
+
+
+router.get('/profile',authMiddleware.authCaptain , captainController.getCaptainProfile);
+
+router.get('/logout',authMiddleware.authCaptain , captainController.logoutCaptain);
 
 
  module.exports = router;

d66055b571819c0f421e04a477b8245b2622b561
Mudit Kubrey <94749969+DeadlyRanger@users.noreply.github.com>
2025-01-03 19:07:04 +0530

    Update Start.jsx

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -1,21 +1,18 @@
+
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Start = () => {
   return (
-  <div>
-  <div className='bg-cover bg-[url(https://plus.unsplash.com/premium_photo-1690958385391-76844034f557?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dHJhZmZpY3xlbnwwfHwwfHx8MA%3D%3D) ] flex flex-col justify-between w-full h-screen pt-5 bg-red-400  bg- pl -5'>
-   <img  classname = 'ml-8 w-14 ' src="https://pngimg.com/d/uber_PNG24.png" alt="" />
-  <div className='px-4 py-4 pb-5 bg-white'>
-    <h2  className='text-3xl font-bold'>Get Started with Uber</h2>
-    <Link  to = '/login' className='flex justify-center py-2 mt-5 text-center text-white bg-black rounded inlinw-full'>Continue</Link>
-
-  </div>
-
-  </div>
-
-
-  </div>
+    <div>
+      <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1619059558110-c45be64b73ae?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8 flex justify-between flex-col w-full'>
+        <img className='w-16 ml-8' src="https://cdn-assets-eu.frontify.com/s3/frontify-enterprise-files-eu/eyJwYXRoIjoid2VhcmVcL2ZpbGVcLzhGbTh4cU5SZGZUVjUxYVh3bnEyLnN2ZyJ9:weare:F1cOF9Bps96cMy7r9Y2d7affBYsDeiDoIHfqZrbcxAw?width=1200&height=417" alt="" />
+        <div className='px-4 py-4 pb-8 bg-white'>
+          <h2 className='text-[30px] font-semibold'>Get Started with Uber</h2>
+          <Link to='/login' className='flex items-center justify-center w-full py-3 mt-5 text-white bg-black rounded-lg'>Continue</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
